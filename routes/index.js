2a1c8046ae36beaa458f80e14eca6ec6328f7170
guantao.hu@jimubox.com <18146525429@163.com>
2015-11-17 00:58:14 +0800

    view added, home and store page done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '主页' });
+  res.render('home', { title: '主页' });
 });
 
 module.exports = router;
--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET store page. */
 router.get('/', function(req, res, next) {
-  res.render('store', { title: '我是店家' });
+  res.render('store/store', { title: '我是店家' });
 });
 
 module.exports = router;
