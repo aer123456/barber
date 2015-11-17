8f4b535fe18ca7b07c7bff871127d13b2ced249e
guantao.hu@jimubox.com <18146525429@163.com>
2015-11-18 01:16:19 +0800

    store pages done, user's page waiting

--- routes/store.js
+++ routes/store.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('store/store', { title: '我是店家' });
 });
 
+/* GET appointment list page. */
+router.get('/appointment', function(req, res, next) {
+  res.render('store/appointment', { title: '预约列表' });
+});
+
+/* GET appointment list page. */
+router.get('/customer_list', function(req, res, next) {
+  res.render('store/customer_list', { title: '会员列表' });
+});
+
+
+
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res, next) {
 
 /* GET register page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: '注册' });
+  res.render('customer/register', { title: '注册' });
 });
 
 /* GET change password page. */
 router.get('/changePasswd', function(req, res, next) {
-  res.render('changePasswd', { title: '修改密码'});
+  res.render('customer/changePasswd', { title: '修改密码'});
 });
 
 /* GET user home page. */
@@ -23,22 +23,22 @@ router.get('/home', function(req, res, next) {
 
 /* GET account info page. */
 router.get('/myAccount', function(req, res, next) {
-  res.render('myAccount', { title: '我的账户' });
+  res.render('customer/myAccount', { title: '我的账户' });
 });
 
 /* GET appointing page. */
 router.get('/appointing', function(req, res, next) {
-  res.render('appointing', { title: '预约'});
+  res.render('customer/appointing', { title: '预约'});
 });
 
 /* GET normal user page. */
 router.get('/normal', function(req, res, next) {
-  res.render('normal', { title: '普通用户' });
+  res.render('customer/normal', { title: '普通用户' });
 });
 
 /* GET vip user page. */
 router.get('/vip', function(req, res, next) {
-  res.render('vip', { title: 'vip用户' });
+  res.render('customer/vip', { title: 'vip用户' });
 });
 
 module.exports = router;
