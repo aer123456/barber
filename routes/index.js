1e06366d786a6f7b3d6234420e710dae35812893
guantao.hu@jimubox.com <18146525429@163.com>
2015-11-22 22:55:24 +0800

    views done, waiting for data changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: '主页' });
 });
 
+/* GET hair style page. */
+router.get('/hair_style', function(req, res, next) {
+  res.render('hairstyle', { title: '最新发型' });
+});
+
+/* GET all hair style page. */
+router.get('/hair_all', function(req, res, next) {
+  res.render('hair_all', { title: '所有发型' });
+});
+
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: '发生错误了' });
+});
+
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -11,9 +11,30 @@ router.get('/register', function(req, res, next) {
   res.render('customer/register', { title: '注册' });
 });
 
+/* GET register success page. */
+router.get('/reg_success', function(req, res, next) {
+  res.render('customer/reg_success', { title: '注册成功' });
+});
+
+/* GET first charge page. */
+router.get('/first_charge', function(req, res, next) {
+  res.render('customer/charge_first', { title: '首次充值' });
+});
+
+/* GET charge page. */
+router.get('/charge', function(req, res, next) {
+  res.render('customer/charge', { title: '充值' });
+});
+router.get('/charge.do', function(req, res, next) {
+  res.render('customer/charge_do', { title: '充值成功' });
+});
+
 /* GET change password page. */
 router.get('/changePasswd', function(req, res, next) {
   res.render('customer/changePasswd', { title: '修改密码'});
+});
+router.get('/change_do', function(req, res, next) {
+  res.render('customer/change_do', { title: '修改密码成功'});
 });
 
 /* GET user home page. */
@@ -30,10 +51,23 @@ router.get('/myAccount', function(req, res, next) {
 router.get('/appointing', function(req, res, next) {
   res.render('customer/appointing', { title: '预约'});
 });
+router.get('/appointing_do', function(req, res, next) {
+  res.render('customer/appointing_do', { title: '预约成功'});
+});
 
 /* GET normal user page. */
 router.get('/normal', function(req, res, next) {
   res.render('customer/normal', { title: '普通用户' });
+});
+
+/* GET normal user appointing page. */
+router.get('/normal_appointing', function(req, res, next) {
+  res.render('customer/normal_appoint', { title: '普通用户预约' });
+});
+
+/* GET normal user appointing success page. */
+router.get('/normal_do', function(req, res, next) {
+  res.render('customer/normal_do', { title: '普通用户预约成功' });
 });
 
 /* GET vip user page. */
